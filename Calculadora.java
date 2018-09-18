058ca93d39d028e7c949650f108e804b40c9f354
Luis FIlipe Sena <felipealexya12@icloud.com>
2018-09-17 21:57:33 -0300

    adicao de novos metodos

--- Calculadora.java
+++ Calculadora.java
@@ -6,4 +6,32 @@ public class Calculadora {
     public int dividir(int num, int den) {
 	return num/den;
     }
+    public int mul(int x, int y){
+         return x*y;
+    }
+    public int dif(int x, int y){
+         return x-y;
+    }
+    public int max(int x, int y){
+    if (x>y){ {
+    return x;
+    }
+    else{  
+      return y;
+    }
+    }
+    }
+    public int min(int x, int y){
+      if (x<y){ {
+    return x;
+    }
+    else{  
+      return y;
+    }
+    }
+    }
+    public int media(int x, int y){
+      return (x+y)/2;
+    }
 }
+
